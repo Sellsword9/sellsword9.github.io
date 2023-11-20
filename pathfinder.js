24ba2cf1f74a58006fba01c808db726fdd43b85a
Sellsword9 <yerayromeroortiz@gmail.com>
2023-11-20 14:16:21 +0100

    First pathfinding algorithm implemented

--- pathfinder.js
+++ pathfinder.js
@@ -13,7 +13,7 @@ function createBoard() {
     }
   }
 }
-function generateRandomPoints(randomConstant) {
+function generateWalls(randomConstant) {
   if (!randomConstant) {
     randomConstant = 5;
   }
@@ -36,5 +36,122 @@ function generateRandomPoints(randomConstant) {
     randomCell.classList.add('wall');
   }
 }
+function generateStartingPoints(startingPoint, endingPoint) {
+  const board = document.getElementById('board');
+  if (startingPoint && endingPoint) {
+    startingPoint.classList.add('path-start');
+    endingPoint.classList.add('path-end');
+    return [startingPoint, endingPoint];
+  } else {
+    let randomCord1 = Math.floor(Math.random() * numRows);
+    let randomCord2 = Math.floor(Math.random() * numCols);
+    point1 = startingPoint ? startingPoint : board.querySelector(`[data-row="${randomCord1}"][data-col="${randomCord2}"]`);
+    randomCord1 = Math.floor(Math.random() * numRows);
+    randomCord2 = Math.floor(Math.random() * numCols);
+    point2 = endingPoint ? endingPoint : board.querySelector(`[data-row="${randomCord1}"][data-col="${randomCord2}"]`);
+
+    point1.classList.add('path-start');
+    point2.classList.add('path-end');
+    return [point1, point2];
+  }
+}
+async function generatePath(start, end, interval = 1000) {
+  console.log("Trace?");
+  const path = findPath(start, end);
+  if (path.length > 0) {
+    for (let i = 0; i < path.length; i++) {
+      const currentCell = path[i];
+      if (!currentCell.classList.contains('path-start') && !currentCell.classList.contains('path-end')) {
+        currentCell.classList.add('path-found');
+        // sleep for 1 s then proceed with the for
+        await sleep(interval);
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+function findPath(start, end) {
+  const queue = [];
+  const visited = new Set();
+  const parent = new Map();
+  queue.push(start);
+  visited.add(start);
+  while (queue.length > 0) {
+    const currentCell = queue.shift();
+    if (currentCell === end) {
+      return constructPath(parent, start, end);
+    }
+    const neighbors = getNeighbors(currentCell);
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighbor = neighbors[i];
+      if (!visited.has(neighbor) && !neighbor.classList.contains('wall')) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+        parent.set(neighbor, currentCell);
+      }
+    }
+  }
+  return [];
+}
+function constructPath(parent, start, end) {
+  const path = [];
+  let currentCell = end;
+  while (currentCell !== start) {
+    path.unshift(currentCell);
+    currentCell = parent.get(currentCell);
+  }
+  return path;
+}
+function getNeighbors(cell) {
+  const neighbors = [];
+  const row = parseInt(cell.dataset.row);
+  const col = parseInt(cell.dataset.col);
+  const north = document.querySelector(`[data-row="${row - 1}"][data-col="${col}"]`);
+  const south = document.querySelector(`[data-row="${row + 1}"][data-col="${col}"]`);
+  const east = document.querySelector(`[data-row="${row}"][data-col="${col + 1}"]`);
+  const west = document.querySelector(`[data-row="${row}"][data-col="${col - 1}"]`);
+  if (north) {
+    neighbors.push(north);
+  }
+  if (south) {
+    neighbors.push(south);
+  }
+  if (east) {
+    neighbors.push(east);
+  }
+  if (west) {
+    neighbors.push(west);
+  }
+  return neighbors;
+
+}
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function resetBoard() {
+  createBoard();
+  generateWalls();
+  generateStartingPoints(false, false);
+}
+function startInfinitePathfinding() {
+  let c = 1;
+  while (true) {
+    generatePath(points[0], points[1], 200)
+    resetBoard();
+    if (c > 100) { break; }
+    c++;
+  }
+}
 createBoard();
-generateRandomPoints();
+generateWalls();
+points = generateStartingPoints(false, false);
+document.getElementById('start-pathfinding').addEventListener('click', () => {
+  generatePath(points[0], points[1], 200);
+});
+document.getElementById('reset').addEventListener('click', resetBoard);
+// document.getElementById('start-pathfinding-infinite').addEventListener('click', () => {
+//   resetBoard();
+//   startInfinitePathfinding();
+// });
